85cb188182e5efa789bd4e7f4593f32d35e2ae28
barnabasp <barnabas.porfy@gmail.com>
2021-03-09 08:21:31 +0100

    First hw

---
+++ 1st_hw/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/barnabasp/Documents/Code/mod_sci_met_phy/1st_hw
+# Build directory: /Users/barnabasp/Documents/Code/mod_sci_met_phy/1st_hw/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
