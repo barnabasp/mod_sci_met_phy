0da3b3567bccd9fe60c84b2d6de46d2590f3f793
barnabasp <barnabas.porfy@gmail.com>
2021-03-09 16:52:43 +0100

    Newton iterator defined, needs improvement, test case fails

--- 1st_hw/main.cpp
+++ 1st_hw/main.cpp
@@ -3,10 +3,26 @@
 Newton iterator based on: https://en.wikipedia.org/wiki/Newton%27s_method
 
 */
-
-
-
+template<typename F, typename D, typename X>
+//F type of function
+//D type of derivative 
+//X type of initial guess
+X newtonIt(F f, D df, X x0)
+{
+    X x1 = x0 - f(x0)/df(x0);
+    return x1;
+}
+double func(double x)
+{
+    return x*x - 612.0;
+}
+double df(double x)
+{
+    return 2.0*x;
+}
 int main() {
-
+    double x0 = 10.0;
+    double test = newtonIt(func,df,x0);
+    std::cout << test;
     return 0;
 }
