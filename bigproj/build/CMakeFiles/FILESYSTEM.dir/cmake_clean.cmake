b22944bd17a37e44e953513227da47b3d71081e5
barnabasp <barnabas.porfy@gmail.com>
2021-03-09 07:58:12 +0100

    First test for big project

---
+++ bigproj/build/CMakeFiles/FILESYSTEM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FILESYSTEM.dir/main.cpp.o"
+  "FILESYSTEM.pdb"
+  "FILESYSTEM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FILESYSTEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
