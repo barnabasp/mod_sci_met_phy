c01ff6c354f7769d84d23312457a0ffe2b07693d
barnabasp <barnabas.porfy@gmail.com>
2021-03-09 16:59:53 +0100

    Iterator now gives back test case from slides. Further improvements for extra points are needed.

--- 1st_hw/main.cpp
+++ 1st_hw/main.cpp
@@ -3,13 +3,20 @@
 Newton iterator based on: https://en.wikipedia.org/wiki/Newton%27s_method
 
 */
-template<typename F, typename D, typename X>
+template<typename F, typename D, typename X, typename E>
 //F type of function
 //D type of derivative 
 //X type of initial guess
-X newtonIt(F f, D df, X x0)
+//E type of precision (epsilon)
+X newtonIt(F f, D df, X x0, E eps)
 {
+    //newton iterator itself
     X x1 = x0 - f(x0)/df(x0);
+    while(abs(x1-x0) > eps)
+    {
+        x0 = x1;
+        x1 = x0 - f(x0)/df(x0);
+    }
     return x1;
 }
 double func(double x)
@@ -22,7 +29,7 @@ double df(double x)
 }
 int main() {
     double x0 = 10.0;
-    double test = newtonIt(func,df,x0);
+    double test = newtonIt(func,df,x0,1e-5);
     std::cout << test;
     return 0;
 }
