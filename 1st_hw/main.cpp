7233c933cbd12c729662cf1faabc4319ad6f3671
barnabasp <barnabas.porfy@gmail.com>
2021-03-09 17:16:37 +0100

    Result comparison is working; added extra check to see if derivate is not being too small; comparison could be improved

--- 1st_hw/main.cpp
+++ 1st_hw/main.cpp
@@ -3,21 +3,30 @@
 Newton iterator based on: https://en.wikipedia.org/wiki/Newton%27s_method
 
 */
-template<typename F, typename D, typename X, typename E>
+bool stopIterate(double x1, double x0, double tol);
+template<typename F, typename D, typename X, typename T, typename E>
 //F type of function
 //D type of derivative 
 //X type of initial guess
-//E type of precision (epsilon)
-X newtonIt(F f, D df, X x0, E eps)
+//E type of precision (epsilon) for derivative
+//T type of tolerance (accuracy)
+X newtonIt(F f, D df, X x0, T tol, E eps)
 {
     //newton iterator itself
     X x1 = x0 - f(x0)/df(x0);
-    while(abs(x1-x0) > eps)
+    while(stopIterate(x1,x0,tol))
     {
+        //if derivative is too small, stop ()
+        if(abs(df(x0)) < eps) break;
         x0 = x1;
         x1 = x0 - f(x0)/df(x0);
     }
     return x1;
+}
+//Function to stop the iteration if difference is less than tolerance
+bool stopIterate(double x1, double x0, double tol)
+{
+    return abs(x1-x0) > tol;
 }
 double func(double x)
 {
@@ -29,7 +38,9 @@ double df(double x)
 }
 int main() {
     double x0 = 10.0;
-    double test = newtonIt(func,df,x0,1e-5);
-    std::cout << test;
+    double test = newtonIt(func,df,x0,1e-7,1e-15);
+    std::cout.precision(25);
+    std::cout << test << std::endl;
+    std::cout << test - 24.738633753705963298928 << std::endl;
     return 0;
 }
