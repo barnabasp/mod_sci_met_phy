fdfbc13fcadf35ae6e8610e448159f544a2003cd
barnabasp <barnabas.porfy@gmail.com>
2021-03-23 23:39:43 +0100

    Added more functions, operators; divison is still missing, could make normalize working, but first want to try with division operator

--- 2nd_hw/main.cpp
+++ 2nd_hw/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     Vector2<int> v1(1,2);
     Vector2<int> v2(2,3);
+    int a = 5;
     std::cout<<v1 <<std::endl;
-    std::cout<<v2 << std::endl;
+    std::cout<<v2/a << std::endl;
 }
--- 2nd_hw/vector2.h
+++ 2nd_hw/vector2.h
@@ -7,6 +7,11 @@ class Vector2
         Vector2(T a, T b);
         void setX(T newX);
         void setY(T newY);
+        double dot(const Vector2<T>& a, const Vector2<T>& b);
+        double length(const Vector2<T>& a);
+        double sqlength(const Vector2<T>& a);
+        Vector2<T> norm(const Vector2<T>& a);
+
         Vector2<T> operator-(Vector2<T>& a)
         {
             return Vector2<T>(x - a.x, y - a.y);
@@ -15,11 +20,21 @@ class Vector2
         {
             return Vector2<T>(x + a.x, y + a.y);
         };
+        Vector2<T> operator/=(T a)
+        {
+            return Vector2<T>(x/a, y/a);
+        }
         friend std::ostream& operator<<(std::ostream& stream, const Vector2<T>& a)
         {
             stream << a.x << " " << a.y;
             return stream;
         };
+        friend std::istream& operator>>(std::istream& stream, const Vector2<T>& a)
+        {
+            stream >> a.x;
+            stream >> a.y;
+            return stream;
+        }
      private:
         T x;
         T y;
@@ -39,4 +54,24 @@ template<typename T>
 void Vector2<T>::setY(T newY)
 {
     y = newY;
+}
+template<typename T>
+double dot(const Vector2<T>& a, const Vector2<T>& b)
+{
+    return a.x * b.y + a.y * b.x;
+}
+template<typename T>
+double length(const Vector2<T>& a)
+{
+    return sqrt(a.x*a.x + a.y * a.y);
+}
+template<typename T>
+double sqlength(const Vector2<T>& a)
+{
+    return a.x*a.x + a.y*a.y;
+}
+template<typename T>
+Vector2<T> normalize(const Vector2<T>& a)
+{
+    return a/Vector2<T>::length(a);
 }
