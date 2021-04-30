50e6a19250b74cd69ffef9520451892dcd8c8244
barnabasp <barnabas.porfy@gmail.com>
2021-04-30 10:02:20 +0200

    added and extended explore function; some basic information gathering (file sizes, paths) is done

--- bigproj/funcs.hpp
+++ bigproj/funcs.hpp
@@ -4,21 +4,33 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <vector>
 namespace fs = std::filesystem;
 
 class System_search
 {
-    private:
-    std::string pathing;
-    public:
+private:
+    //private variables 
+    std::string _pathing;
+    //containers for file properties
+    std::vector<fs::path> _files;
+    std::vector<int> _fileSizes;
+    
+    //containers for folder properties
+    std::vector<fs::path> _folders;
+    std::vector<int> _folderSizes;
+    std::vector<int> _folderContent;
+public:
+    //constructors
     System_search() = default;
     System_search(std::string strPath)
     {
-        pathing = strPath;
+        _pathing = strPath;
     };
+    //simple function to check the filesystem
     void startup()
     {
-        fs::path currPath(pathing);
+        fs::path currPath(_pathing);
         std::cout << "root name  " << currPath.root_name() << std::endl;
         std::cout << "root directory  " << currPath.root_directory() << std::endl;
         std::cout << "root path  " << currPath.root_path() << std::endl;
@@ -27,6 +39,31 @@ class System_search
         std::cout << "filename  " << currPath.filename() << std::endl;
         std::cout << "stem  " << currPath.stem() << std::endl;
         std::cout << "extension  " << currPath.extension() << std::endl;
+    }
+    //main function that gathers information on the folders and files
+    void explore()
+    {
+        //looping through the paths
+        for(auto& p: fs::recursive_directory_iterator(_pathing))
+        {
+            //std::cout << p.path() << '\n';
+            //std::cout << fs::is_directory(p) << '\n';
+            //check if path is a directory
+            if(fs::is_directory(p))
+            {
+                if(fs::is_empty(p)) _folderContent.push_back(0);
+                std::cout << p.path() << '\n';
+                continue;
+            }
+            //if path is not a directory
+            else
+            {
+                //std::cout << fs::file_size(p) << '\n';
+                _files.push_back(p.path());
+                _fileSizes.push_back(fs::file_size(p.path()));
+                //count the files in this folder then go deeper-> add to content of folder
+            }
+        }
     }
 };
 
--- bigproj/main.cpp
+++ bigproj/main.cpp
@@ -6,15 +6,7 @@
 
 int main()
 {
-    /*
-    fs::create_directories("sandbox/a/b");
-    std::ofstream("sandbox/file1.txt");
-    std::ofstream("sandbox/file2.txt");
-    for(auto& p: fs::directory_iterator("sandbox"))
-        std::cout << p.path() << '\n';
-    fs::remove_all("sandbox");
-    */
     System_search temp("/Users/barnabasp/Documents/Code/mod_sci_met_phy/bigproj/");
-    temp.startup();
+    //temp.startup();
+    temp.explore();
 }
-//space visibility
